2273397e784e685b09ad580eac896add6419d450
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-07-21 13:28:08 +0200

    there we go

--- packages/client/src/chainhead/follow.ts
+++ packages/client/src/chainhead/follow.ts
@@ -17,82 +17,87 @@ export function follow(
     string,
     FollowEventWithoutRuntime | FollowEventWithRuntime
   >,
+): (
   withRuntime: true,
   cb: (event: FollowEventWithRuntime) => void,
-): FollowResponse
+) => FollowResponse
 export function follow(
   request: ClientRequest<
     string,
     FollowEventWithoutRuntime | FollowEventWithRuntime
   >,
+): (
   withRuntime: false,
   cb: (event: FollowEventWithoutRuntime) => void,
-): FollowResponse
+) => FollowResponse
 export function follow(
   request: ClientRequest<
     string,
     FollowEventWithoutRuntime | FollowEventWithRuntime
   >,
-  withRuntime: boolean,
-  cb:
-    | ((event: FollowEventWithoutRuntime) => void)
-    | ((event: FollowEventWithRuntime) => void),
-): FollowResponse {
-  const _genesisHash = new Promise<string>((res) => {
-    request("chainHead_unstable_genesisHash", [], res)
-  })
-
-  let unfollow: () => void = () => {}
-  let followSubscription: Promise<string> | string = new Promise((res) => {
-    unfollow = request(
-      "chainHead_unstable_follow",
-      [withRuntime],
-      (result: string, follow) => {
-        follow(
-          (event, done) => {
-            if (followFinalEvents.has(event.event)) done()
-            cb(event as any)
-          },
-          result,
-          "chainHead_unstable_unfollow",
-        )
-        followSubscription = result
-        res(result)
-      },
-    )
-  })
-
-  const fRequest = <T, TT>(
-    method: string,
-    params: Array<any>,
-    cb: ClientRequestCb<T, TT>,
-  ) => {
-    const req = request as unknown as ClientRequest<T, TT>
-    if (typeof followSubscription === "string")
-      return req(method, [followSubscription, ...params], cb)
-
-    let isAborted = false
-    let onCancel = () => {
-      isAborted = true
-    }
-
-    followSubscription.then((sub) => {
-      if (isAborted) return
-      onCancel = req(method, [sub, ...params], cb)
+) {
+  return (
+    withRuntime: boolean,
+    cb:
+      | ((event: FollowEventWithoutRuntime) => void)
+      | ((event: FollowEventWithRuntime) => void),
+  ): FollowResponse => {
+    const _genesisHash = new Promise<string>((res) => {
+      request("chainHead_unstable_genesisHash", [], res)
     })
 
-    return () => {
-      onCancel()
-    }
-  }
+    let unfollow: () => void = () => {}
+    let followSubscription: Promise<string> | string = new Promise((res) => {
+      unfollow = request(
+        "chainHead_unstable_follow",
+        [withRuntime],
+        (result: string, follow) => {
+          follow(
+            (event, done) => {
+              if (followFinalEvents.has(event.event)) done()
+              cb(event as any)
+            },
+            result,
+            "chainHead_unstable_unfollow",
+          )
+          followSubscription = result
+          res(result)
+        },
+      )
+    })
 
-  return {
-    genesisHash: () => _genesisHash,
-    unfollow,
-    body: createBodyFn(fRequest),
-    call: createCallFn(fRequest),
-    header: createHeaderFn(fRequest),
-    storage: createStorageFn(fRequest, request),
-    unpin: createUnpinFn(fRequest),
+    const fRequest = <T, TT>(
+      method: string,
+      params: Array<any>,
+      cb: ClientRequestCb<T, TT>,
+    ) => {
+      const req = request as unknown as ClientRequest<T, TT>
+      if (typeof followSubscription === "string")
+        return req(method, [followSubscription, ...params], cb)
+
+      let isAborted = false
+      let onCancel = () => {
+        isAborted = true
+      }
+
+      followSubscription.then((sub) => {
+        if (isAborted) return
+        onCancel = req(method, [sub, ...params], cb)
+      })
+
+      return () => {
+        onCancel()
+      }
+    }
+
+    return {
+      genesisHash: () => _genesisHash,
+      unfollow,
+      body: createBodyFn(fRequest),
+      call: createCallFn(fRequest),
+      header: createHeaderFn(fRequest),
+      storage: createStorageFn(fRequest, request),
+      unpin: createUnpinFn(fRequest),
+    }
   }
 }
--- packages/client/src/chainhead/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./follow"
-export * from "./types"
