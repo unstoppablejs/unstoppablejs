1bc0d56120199fdbd52e8acd2150591b81a1533a
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-02-12 17:11:52 +0100

    fix(scale-ts): Tuple encoding cap array iput

--- packages/scale-ts/src/tests/Tuple.test.ts
+++ packages/scale-ts/src/tests/Tuple.test.ts
@@ -1,10 +1,15 @@
 import { Tuple, compact, bool } from "../"
-import { testCodec } from "./test-utils"
+import { testCodec, toHex } from "./test-utils"
 
-const tester = testCodec(Tuple(compact, bool))
+const codec = Tuple(compact, bool)
+const tester = testCodec(codec)
 
 describe("Tuple", () => {
   it("works", () => {
     tester([3, false], "0x0c00")
+  })
+
+  it("still works", () => {
+    expect(toHex(codec.enc([3, false, true] as any))).toEqual("0x0c00")
   })
 })
